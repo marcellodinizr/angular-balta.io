16944cc64773da9dacf0a6db44a05e75517a3bdb
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-14 11:07:01 -0300

    general commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public todos: Todo[] = [];
